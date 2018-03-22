1ee9598eb5b2fc1b315ba97baf70663b3b7cf468
PutraRandy <33760355+PutraRandy@users.noreply.github.com>
2018-03-22 23:21:59 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -41,10 +41,10 @@ class LINE:
       raise Exception("invalid arguments")
 
     self.authToken = self.Talk.authToken
-    self._headers = {'X-Line-Application':'DESKTOPMAC 10.10.2-YOSEMITE-x64','X-Line-Access': self.authToken, 'User-Agent': 'Line/8.0.0'}
+    self._headers = {'X-Line-Application':'DESKTOPMAC 8.2.2  12.0.2','X-Line-Access': self.authToken, 'User-Agent': 'Line/2018.07332.2233.Tanduri/760.1.6 Tanysyz/18.0.0'}
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0',
+              'X-Line-Application': 'DESKTOPMAC 8.2.2  ',
               'X-Line-Access': self.authToken, 
               'User-Agent': 'Line/8.0.0'
    }
